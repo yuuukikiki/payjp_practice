422dbae888459d92ed72c5d1e8854edf7d517df3
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-08-28 14:28:08 +0900

    Orderモデル〜テストの実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,23 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
+  end
+
+end
